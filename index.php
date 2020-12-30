9474de617d7dd29b1a046f20625f03246bcb8b72
Rajesh Mondal <rajesh01749@gmail.com>
2020-12-30 19:59:55 +0600

    working with single post view

--- index.php
+++ index.php
@@ -28,7 +28,9 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
-                        <h2 class="post-title"><?php the_title(); ?>></h2>
+                        <h2 class="post-title">
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        </h2>
                     </div>
                 </div>
                 <div class="row">
@@ -50,7 +52,13 @@
                             }
                             ?>
                         </p>
-                        <?php the_excerpt(); ?>
+                        <?php
+                        if(is_single()){
+                            the_content();
+                        }else{
+                            the_excerpt();
+                        }
+                        ?>
 
                     </div>
                 </div>
