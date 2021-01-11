6fbaef5bbcfe90b08cdf1a2fc76c53a0280d7eab
Rajesh Mondal <rajesh01749@gmail.com>
2021-01-11 23:19:45 +0600

    sidebar check

--- single.php
+++ single.php
@@ -1,104 +1,116 @@
+<?php
+$alpha_layout_class = "col-md-8";
+$alpha_text_class   = "";
+if ( ! is_active_sidebar( "sidebar-1" ) ) {
+	$alpha_layout_class = "col-md-10 offset-md-1";
+	$alpha_text_class   = "text-center";
+}
+?>
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
-<?php get_template_part("/template-parts/common/hero"); ?>
+<?php get_template_part( "/template-parts/common/hero" ); ?>
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
+            <div class="<?php echo $alpha_layout_class; ?>">
                 <div class="posts">
-		            <?php
-		            while(have_posts()){
-			            the_post();
-			            ?>
+					<?php
+					while ( have_posts() ) {
+						the_post();
+						?>
                         <div <?php post_class(); ?>>
                             <div class="container">
                                 <div class="row">
                                     <div class="col-md-12">
-                                        <h2 class="post-title">
-								            <?php the_title(); ?>
+                                        <h2 class="post-title <?php echo $alpha_text_class; ?>">
+											<?php the_title(); ?>
                                         </h2>
-                                        <p class="">
+                                        <p class="<?php echo $alpha_text_class; ?>">
                                             <strong><?php the_author(); ?></strong><br/>
-								            <?php echo get_the_date(); ?>
+											<?php echo get_the_date(); ?>
                                         </p>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-12">
                                         <p>
-								            <?php
-								            if (has_post_thumbnail()){
-									            $thumbnail_url = get_the_post_thumbnail_url(null,"large");
-									            printf('<a href="%s" data-featherlight="image">', $thumbnail_url);
-									            the_post_thumbnail("large",array("class"=>"img-fluid"));
-									            echo '</a>';
-								            }
-								            the_content();
-								            wp_link_pages();
+											<?php
+											if ( has_post_thumbnail() ) {
+												$thumbnail_url = get_the_post_thumbnail_url( null, "large" );
+												printf( '<a href="%s" data-featherlight="image">', $thumbnail_url );
+												the_post_thumbnail( "large", array( "class" => "img-fluid" ) );
+												echo '</a>';
+											}
+											the_content();
+											wp_link_pages();
 
-								            /* next_post_link();
-								            echo "<br/>";
-								            previous_post_link(); */
+											/* next_post_link();
+											echo "<br/>";
+											previous_post_link(); */
 
-								            ?>
+											?>
                                         </p>
                                     </div>
 
                                     <div class="authorsection">
                                         <div class="row">
                                             <div class="col-md-2 authorimage">
-                                                <?php
-                                                echo get_avatar(get_the_author_meta("id"));
-                                                ?>
+												<?php
+												echo get_avatar( get_the_author_meta( "id" ) );
+												?>
                                             </div>
                                             <div class="col-md-10">
                                                 <h4>
-                                                    <?php echo get_the_author_meta("display_name"); ?>
+													<?php echo get_the_author_meta( "display_name" ); ?>
                                                 </h4>
                                                 <P>
-                                                    <?php echo get_the_author_meta("description"); ?>
+													<?php echo get_the_author_meta( "description" ); ?>
                                                 </P>
                                             </div>
                                         </div>
                                     </div>
 
-						            <?php if (comments_open()): ?>
+									<?php if ( comments_open() ): ?>
                                         <div class="col-md-10 offset-md-1">
-								            <?php
-								            comments_template();
-								            ?>
+											<?php
+											comments_template();
+											?>
                                         </div>
-						            <?php endif; ?>
+									<?php endif; ?>
                                 </div>
                             </div>
                         </div>
 
-			            <?php
-		            }
-		            ?>
+						<?php
+					}
+					?>
 
                     <div class="container post-pagination">
                         <div class="row">
                             <div class="col-md-4"></div>
                             <div class="col-md-8">
-					            <?php
-					            the_posts_pagination(array(
-						            "screen_reader_text"=>' ',
-						            "prev_text" => "New Posts",
-						            "next_test" => "Old Posts"
-					            ));
-					            ?>
+								<?php
+								the_posts_pagination( array(
+									"screen_reader_text" => ' ',
+									"prev_text"          => "New Posts",
+									"next_test"          => "Old Posts"
+								) );
+								?>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <?php
-                if(is_active_sidebar("sidebar-1")){
-                    dynamic_sidebar("sidebar-1");
-                }
-                ?>
-            </div>
+			<?php
+			if ( is_active_sidebar( "sidebar-1" ) ):
+				?>
+                <div class="col-md-4">
+					<?php
+					if ( is_active_sidebar( "sidebar-1" ) ) {
+						dynamic_sidebar( "sidebar-1" );
+					}
+					?>
+                </div>
+			<?php endif; ?>
         </div>
     </div>
 
